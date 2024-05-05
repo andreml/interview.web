943a8fbad206e1e6333fcb792324024d4307ffff
andre <andre@DESKTOP-H0HQ56M>
2024-05-04 23:07:19 -0300

    Add project files.

---
+++ interview.web/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace interview.web.Models
+{
+    public class LoginViewModel
+    {
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ interview.web/Models/UsuarioViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace interview.web.Models
+{
+    public class UsuarioViewModel
+    {
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public string Perfil { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
