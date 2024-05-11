fd2bc13bb200f05e4add8a5c2fc0b4dbea9b4095
lira.jonas88 <lira.jonas88@gmail.comm>
2024-05-11 20:23:54 -0300

    Implementar get áreas de conhecimento

--- interview.web/Controllers/AreaConhecimentoController.cs
+++ interview.web/Controllers/AreaConhecimentoController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using interview.web.App.Interfaces;
+using interview.web.Config;
+using interview.web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using static interview.web.Models.Enums.Enumerator;
 
 namespace interview.web.Controllers
 {
-    public class AreaConhecimentoController : Controller
+    public class AreaConhecimentoController : BaseController
     {
-        public IActionResult Index()
+        private readonly IGetServices<List<AreaConhecimentoResponseViewModel>> _get;
+        private readonly IPostServices<string> _post;
+        private readonly AppConfig _config;
+        public AreaConhecimentoController(IGetServices<List<AreaConhecimentoResponseViewModel>> get,
+                                 IPostServices<string> post,
+                                 IOptions<AppConfig> options)
         {
-            return View();
+            _get = get;
+            _post = post;
+            _config = options.Value;
+        }
+        public async Task<IActionResult> Index([FromServices] IMemoryCache cache)
+        {
+            try
+            {
+                var token = this.GetToken(cache);
+                string url = _config.Url + "AreaConhecimento";
+                var response = await _get.GetCustomAsync(url, token);
+                return View(response);
+            }
+            catch (Exception e)
+            {
+                ViewBag.Alert = Utility.Utils.ShowAlert(Alerts.Error, e.Message);
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        private string GetToken(IMemoryCache cache)
+        {
+            string? token = cache.Get("token")?.ToString();
+            if (token is null) 
+                throw new Exception("Sessão expirou");
+            return token;
         }
     }
 }
---
+++ interview.web/Models/AreaConhecimentoViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace interview.web.Models
+{
+    public class AreaConhecimentoViewModel
+    {
+
+    }
+
+    public class AreaConhecimentoResponseViewModel
+    {
+        public Guid? id { get; set; }
+        public string? descricao { get; set; }
+        public int? perguntasCadastradas { get; set; }
+
+    }
+
+}
