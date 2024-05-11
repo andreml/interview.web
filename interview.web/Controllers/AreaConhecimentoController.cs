4007fcfd93cc4a94629e815dd15d08699d9bf886
lira.jonas88 <lira.jonas88@gmail.comm>
2024-05-11 18:54:51 -0300

    Inclusão de controllers e views

---
+++ interview.web/Controllers/AreaConhecimentoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace interview.web.Controllers
+{
+    public class AreaConhecimentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ interview.web/Controllers/QuestionarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace interview.web.Controllers
+{
+    public class QuestionarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
